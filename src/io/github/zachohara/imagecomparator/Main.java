96fd20b24d72fe55d62c9f006bbdebecff16ba1c
Zach Ohara <zjohara@gmail.com>
2015-05-31 23:09:39 -0700

    Renamed Launcher to Main
    also added some stuff

--- src/io/github/zachohara/imagecomparator/Main.java
+++ src/io/github/zachohara/imagecomparator/Main.java
@@ -1,9 +1,13 @@
 package io.github.zachohara.imagecomparator;
 
-public class Launcher {
+
+public class Main {
 
 	public static void main(String[] args) {
 		FileSelector filedialog = new FileSelector();
+		ImageComparator comparator = new ImageComparator(filedialog.getFiles());
+		comparator.compareAll();
+		System.exit(0);
 	}
 
 }
