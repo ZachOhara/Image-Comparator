f7bc0b243b0398b039a4379881e87a2ae61ec430
Zach Ohara <zjohara@gmail.com>
2015-06-04 13:18:04 -0700

    Fleshed out ImagePanel

--- src/io/github/zachohara/imagecomparator/gui/ImagePanel.java
+++ src/io/github/zachohara/imagecomparator/gui/ImagePanel.java
@@ -1,9 +1,16 @@
 package io.github.zachohara.imagecomparator.gui;
 
-import java.awt.Component;
-import java.awt.image.BufferedImage;
+import io.github.zachohara.imagecomparator.image.Image;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -12,19 +19,75 @@ public class ImagePanel extends JPanel {
 	private JPanel infoPanel;
 	private JLabel filename;
 	private JLabel dimension;
-	private BufferedImage image;
+	private Image image;
 	private JComponent componentImage;
+	private Component rigidArea;
+	
+	
+	private static final int FONT_SIZE = 17;
+	private static final Font INFO_FONT = new Font("Info panel font", Font.PLAIN, FONT_SIZE);
 	
 	private static final long serialVersionUID = 1L;
 
+	/*
+	 * 
+		this.leftPanel = new JPanel();
+		this.rightPanel = new JPanel();
+		this.handleWindowResize();
+		this.leftPanel.setLayout(new BorderLayout());
+		this.rightPanel.setLayout(new BorderLayout());
+		this.leftPanel.setBackground(Color.BLUE);
+		this.rightPanel.setBackground(Color.RED);
+		this.window.add("West", this.leftPanel);
+		this.window.add("East", this.rightPanel);
+	 */
+	
 	public ImagePanel() {
 		super();
-		// TODO
+		this.setLayout(new BorderLayout());
+		this.formatInfoPanel();
+	}
+	
+	public ImagePanel(Color c) {
+		this();
+		JPanel p = new JPanel();
+		p.setBackground(c);
+		this.add(p);
+		this.repaint();
+	}
+	
+	public void resize(int width) {
+		if (this.rigidArea != null)
+			this.remove(this.rigidArea);
+		this.rigidArea = Box.createHorizontalStrut(width);
+		this.add(this.rigidArea);
+		this.repaint();
+	}
+	
+	private void formatInfoPanel() {
+		this.infoPanel = new JPanel();
+		this.infoPanel.setLayout(new BoxLayout(this.infoPanel, BoxLayout.Y_AXIS));
+		this.filename = new JLabel("<<filename here>>");
+		this.dimension = new JLabel("<<dimensions here>>");
+		this.formatInfoText(this.filename);
+		this.formatInfoText(this.dimension);
+		this.add("North", this.infoPanel);
+	}
+	
+	private void formatInfoText(JLabel panel) {
+		panel.setFont(INFO_FONT);
+		panel.setAlignmentX(CENTER_ALIGNMENT);
+		this.infoPanel.add(panel);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame win = new JFrame();
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		win.setSize(500, 500);
+		win.setLocationRelativeTo(null);
+		ImagePanel p = new ImagePanel();
+		win.add(p);
+		win.setVisible(true);
 	}
 
 }
