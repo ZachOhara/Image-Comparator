4549ebf0dab7fa30c74f59bed7f79e4982687f92
Zach Ohara <zjohara@gmail.com>
2015-06-04 13:18:17 -0700

    Reflected ImagePanel changes to Window

--- src/io/github/zachohara/imagecomparator/gui/Window.java
+++ src/io/github/zachohara/imagecomparator/gui/Window.java
@@ -1,16 +1,15 @@
 package io.github.zachohara.imagecomparator.gui;
 
-import io.github.zachohara.imagecomparator.image.Image;
-
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,12 +17,10 @@ import javax.swing.JPanel;
 public class Window {
 
 	private JFrame window;
-	private JPanel leftPanel;
-	private JPanel rightPanel;
-	private JPanel leftInfoPanel;
-	private JPanel rightInfoPanel;
-	private Component rigidAreaLeft;
-	private Component rigidAreaRight;
+	private ImagePanel leftPanel;
+	private ImagePanel rightPanel;
+	
+	private static final int BOTTOM_BUTTON_HEIGHT = 50;
 
 	public Window() {
 		this.initializeWinow();
@@ -37,14 +34,8 @@ public class Window {
 	public void handleWindowResize() {
 		System.out.println("Resize! " + System.currentTimeMillis());
 		int newWidth = this.window.getSize().width;
-		if (this.rigidAreaLeft != null)
-			this.leftPanel.remove(this.rigidAreaLeft);
-		if (this.rigidAreaRight != null)
-			this.rightPanel.remove(this.rigidAreaRight);
-		this.rigidAreaLeft = Box.createRigidArea(new Dimension(newWidth / 2, 0));
-		this.rigidAreaRight = Box.createRigidArea(new Dimension(newWidth / 2, 0));
-		this.leftPanel.add(this.rigidAreaLeft);
-		this.rightPanel.add(this.rigidAreaRight);
+		this.leftPanel.resize(newWidth / 2);
+		this.rightPanel.resize(newWidth / 2);
 		this.window.repaint();
 	}
 	
@@ -53,30 +44,30 @@ public class Window {
 		//TODO: handle button presses
 	}
 	
-	public void setImages(Image left, Image right) {
-		this.setLeftImage(left);
-		this.setRightImage(right);
-	}
-	
-	public void setLeftImage(Image left) {
-		this.leftInfoPanel.removeAll();
-		this.leftInfoPanel.add(new JLabel(left.getName()));
-		this.leftInfoPanel.add(new JLabel(left.getDimensionString()));
-		this.leftPanel.add("Center", left.toComponenet());
-		this.leftInfoPanel.repaint();
-		this.leftPanel.repaint();
-		this.window.repaint();
-	}
-	
-	public void setRightImage(Image right) {
-		this.rightInfoPanel.removeAll();
-		this.rightInfoPanel.add(new JLabel(right.getName()));
-		this.rightInfoPanel.add(new JLabel(right.getDimensionString()));
-		this.rightPanel.add("Center", right.toComponenet());
-		this.rightInfoPanel.repaint();
-		this.rightPanel.repaint();
-		this.window.repaint();
-	}
+//	public void setImages(Image left, Image right) {
+//		this.setLeftImage(left);
+//		this.setRightImage(right);
+//	}
+//	
+//	public void setLeftImage(Image left) {
+//		this.leftInfoPanel.removeAll();
+//		this.leftInfoPanel.add(new JLabel(left.getName()));
+//		this.leftInfoPanel.add(new JLabel(left.getDimensionString()));
+//		this.leftPanel.add("Center", left.toComponenet());
+//		this.leftInfoPanel.repaint();
+//		this.leftPanel.repaint();
+//		this.window.repaint();
+//	}
+//	
+//	public void setRightImage(Image right) {
+//		this.rightInfoPanel.removeAll();
+//		this.rightInfoPanel.add(new JLabel(right.getName()));
+//		this.rightInfoPanel.add(new JLabel(right.getDimensionString()));
+//		this.rightPanel.add("Center", right.toComponenet());
+//		this.rightInfoPanel.repaint();
+//		this.rightPanel.repaint();
+//		this.window.repaint();
+//	}
 
 	private void initializeWinow() {
 		this.window = new JFrame("Image Comparator by Zach Ohara");
@@ -91,7 +82,6 @@ public class Window {
 		this.window.setLayout(new BorderLayout());
 		this.formatTitle();
 		this.formatSides();
-		this.formatInfoPanels();
 		this.formatBottom();
 	}
 
@@ -105,44 +95,27 @@ public class Window {
 	}
 	
 	private void formatSides() {
-		this.leftPanel = new JPanel();
-		this.rightPanel = new JPanel();
+		this.leftPanel = new ImagePanel(Color.GREEN);
+		this.rightPanel = new ImagePanel(Color.PINK);
 		this.handleWindowResize();
-		this.leftPanel.setLayout(new BorderLayout());
-		this.rightPanel.setLayout(new BorderLayout());
-		this.leftPanel.setBackground(Color.BLUE);
-		this.rightPanel.setBackground(Color.RED);
 		this.window.add("West", this.leftPanel);
 		this.window.add("East", this.rightPanel);
-	}
-	
-	private void formatInfoPanels() {
-		this.leftInfoPanel = new JPanel();
-		this.rightInfoPanel = new JPanel();
-		this.leftInfoPanel.setLayout(new BoxLayout(this.leftInfoPanel, BoxLayout.Y_AXIS));
-		this.rightInfoPanel.setLayout(new BoxLayout(this.rightInfoPanel, BoxLayout.Y_AXIS));
-		
-		this.leftInfoPanel.add(new JLabel("Image Name Here"));
-		this.rightInfoPanel.add(new JLabel("Image Name Here"));
-		this.leftInfoPanel.add(new JLabel("100x100"));
-		this.rightInfoPanel.add(new JLabel("100x100"));
-		
-		
-		this.leftPanel.add("North", this.leftInfoPanel);
-		this.rightPanel.add("North", this.rightInfoPanel);
 	}
 	
 	private void formatBottom() {
 		JPanel bottom = new JPanel();
 		bottom.setLayout(new BoxLayout(bottom, BoxLayout.Y_AXIS));
-		Button keepBoth = new Button("Keep Both");
-		Button deleteBoth = new Button("Delete Both");
-		keepBoth.addActionListener(new Listener.ButtonListener(this, "keep both"));
-		deleteBoth.addActionListener(new Listener.ButtonListener(this, "delete both"));
 		bottom.setAlignmentX(JPanel.CENTER_ALIGNMENT);
-		bottom.add(keepBoth);
-		bottom.add(deleteBoth);
+		this.formatButton(bottom, "Keep Both");
+		this.formatButton(bottom, "Delete Both");
 		this.window.add("South", bottom);
+	}
+	
+	private void formatButton(JPanel panel, String name) {
+		Button b = new Button(name);
+		b.setPreferredSize(new Dimension(0, BOTTOM_BUTTON_HEIGHT));
+		b.addActionListener(new Listener.ButtonListener(this, name));
+		panel.add(b);
 	}
 
 	public static void main(String[] args) {
