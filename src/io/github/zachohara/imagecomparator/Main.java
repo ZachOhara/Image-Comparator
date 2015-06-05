f8736f97345f83984d8e19e93a5458a06a91fbd8
Zach Ohara <zjohara@gmail.com>
2015-06-05 00:13:47 -0700

    Added GUI to main executable

--- src/io/github/zachohara/imagecomparator/Main.java
+++ src/io/github/zachohara/imagecomparator/Main.java
@@ -1,11 +1,15 @@
 package io.github.zachohara.imagecomparator;
 
+import io.github.zachohara.imagecomparator.gui.Window;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		FileSelector filedialog = new FileSelector();
-		ImageComparator comparator = new ImageComparator(filedialog.getFiles());
+		Window win = new Window();
+		ImageComparator comparator = new ImageComparator(filedialog.getFiles(), win);
+		win.setVisible(true);
 		comparator.compareAll();
 		System.exit(0);
 	}
