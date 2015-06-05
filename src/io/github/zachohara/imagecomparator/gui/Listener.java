22723794c661552185d89f2205c6e3f9a1edc6a9
Zach Ohara <zjohara@gmail.com>
2015-06-04 21:28:30 -0700

    Added mouse listener

--- src/io/github/zachohara/imagecomparator/gui/Listener.java
+++ src/io/github/zachohara/imagecomparator/gui/Listener.java
@@ -4,8 +4,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public abstract class Listener {
+
+	private Window owner;
+	private String buttonName;
+
+	public Listener(Window win, String button) {
+		this.owner = win;
+		this.buttonName = button;
+	}
+
+	public void notifyOwner() {
+		this.owner.handleButtonPress(buttonName);
+	}
 
 	public static class WindowResizeListener extends ComponentAdapter {
 
@@ -20,22 +34,37 @@ public abstract class Listener {
 		}
 
 	}
-	
-	public static class ButtonListener implements ActionListener {
-		
-		private Window owner;
-		private String buttonName;
-		
+
+	public static class ButtonListener extends Listener implements ActionListener {
+
 		public ButtonListener(Window win, String button) {
-			this.owner = win;
-			this.buttonName = button;
+			super(win, button);
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			this.owner.handleButtonPress(this.buttonName);
+			this.notifyOwner();
 		}
-		
+
+	}
+
+	public static class MouseClickListener extends Listener implements MouseListener {
+
+		public MouseClickListener(Window win, String button) {
+			super(win, button);
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			this.notifyOwner();
+		}
+
+		// Ignore all of these:
+		public void mouseEntered(MouseEvent arg0) {}
+		public void mouseExited(MouseEvent arg0) {}
+		public void mousePressed(MouseEvent arg0) {}
+		public void mouseReleased(MouseEvent arg0) {}
+
 	}
 
 }
